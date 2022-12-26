89b4934ea4e346cfdfdb49ffbf57a28e8727c3e9
Tony Bierman <tonybierman@gmail.com>
2022-12-26 07:16:30 -0600

    Add project files.

---
+++ WpfConsole/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfConsole.ViewModels;
+
+namespace WpfConsole
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ConsoleContent dc = new ConsoleContent();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = dc;
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            InputBlock.KeyDown += InputBlock_KeyDown;
+            InputBlock.Focus();
+        }
+
+        void InputBlock_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                dc.ConsoleInput = InputBlock.Text;
+                dc.RunCommand();
+                InputBlock.Focus();
+                Scroller.ScrollToBottom();
+            }
+        }
+    }
+
+
+}
---
+++ WpfConsole/ViewModels/ConsoleContent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfConsole.ViewModels
+{
+    public class ConsoleContent : INotifyPropertyChanged
+    {
+        private string consoleInput = string.Empty;
+        private ObservableCollection<string> consoleOutput = new ObservableCollection<string>() { "WPF Console Emulator..." };
+
+        public string ConsoleInput
+        {
+            get
+            {
+                return consoleInput;
+            }
+            set
+            {
+                consoleInput = value;
+                OnPropertyChanged("ConsoleInput");
+            }
+        }
+
+        public ObservableCollection<string> ConsoleOutput
+        {
+            get
+            {
+                return consoleOutput;
+            }
+            set
+            {
+                consoleOutput = value;
+                OnPropertyChanged("ConsoleOutput");
+            }
+        }
+
+        public void RunCommand()
+        {
+            ConsoleOutput.Add(ConsoleInput);
+
+            // Parse and execute command here
+
+            ConsoleInput = String.Empty;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
